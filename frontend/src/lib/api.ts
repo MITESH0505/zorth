be4a1cc30b77fbc6cac11e0a75bb55c81c19a5be
MITESH RAJPUT <miteshrajput822@gmail.com>
2026-05-29 16:05:54 +0530

    fix next env

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 // const API_URL = "http://localhost:4000/api";
-const API_URL = `$ {import.meta.env.VITE_API_URL}/api`;
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
 export async function getCategories() {
   try {
